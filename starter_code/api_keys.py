3dfd95ba0d242abb52d7973f1ea1df56fea76d30
Stu-Vic <78116599+Stu-Vic@users.noreply.github.com>
2021-03-19 22:09:32 +1100

    update

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "daee9ab149c95f3b43f4d5582b15c03c"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDqXZBljmYUcKzuG_OUj8wWbJsI7tWD7lA"
